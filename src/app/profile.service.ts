d1da268010f97078c2dc28df849b8f173bfd10c5
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-05-24 09:26:35 +0300

    add chnges in ts

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -5,6 +5,7 @@ import { from } from 'rxjs';
 import{Repository} from './repository'
 import { rejects } from 'assert';
 import { BinaryOperatorExpr } from '@angular/compiler';
+import { error } from '@angular/compiler/src/util';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class ProfileService {
   user!: User;
   repository!: Repository;
   repoData =[];
+  newUserData:any = []
 
   constructor(private http:HttpClient) { 
     this.user =new User("","",0,"")
@@ -34,9 +36,23 @@ export class ProfileService {
         this.user.avatar_url= response.avatar_url;
 
         resolve();
+    },
+    error=>{
+      rejects(error)
     })
+    this.http.get<any>("https://api.github.com/users/"+username + "/repos").toPromise().then(response =>{
+      for(let i=0; i<response.length; i++){
+        this.newUserData = new Repository(response[i].name,response[i].updated_at,response[i].language,);
+        this.repoData.push(this.newUserData);
 
+      }
+      resolve()
+    },error=>{
+      rejects(error)
+    }) 
+  
    })
+  return promise;
 
     
   }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { promise } from 'selenium-webdriver';
 import { ProfileService } from '../profile.service';
 
 @Component({
@@ -7,14 +8,21 @@ import { ProfileService } from '../profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  username!: string;
+  profileService!: ProfileService;
 
-  profile:any;
-  constructor(private profileService:ProfileService) {
-    this.profileService.getProfileData().subscribe(profile =>{
-      console.log(profile);
-      this.profile=profile;
+  submitUsername(){
+    this.profileService.getUserData(this.username)
+  }
 
-    });
+  // profile:any;
+  constructor(profileService:ProfileService) {
+    this.profileService = profileService
+    // this.profileService.getProfileData().subscribe(profile =>{
+    //   console.log(profile);
+    //   this.profile=profile;
+
+    // });
    }
 
   ngOnInit(){
